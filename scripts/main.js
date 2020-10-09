bdd4ba8e38c842381db6672a89c4e521699e74ca
David Ojeda <david.ojeda.lopez@gmail.com>
2020-10-08 23:01:29 -0500

    Lazy load substack embed

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
 import '../styles/base.css'
 import '../styles/main.css'
+
+window.addEventListener("load", (event) => {
+  const embed = document.querySelector('[data-id="substack-embed"]');  
+  let options = {    
+    threshold: .75
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+        var ifrm = document.createElement('iframe');
+        ifrm.setAttribute('frameborder', '0');
+        ifrm.setAttribute('scrolling', 'no');
+        ifrm.setAttribute('title', 'Widget de Substack, el servicio para newsletters que utilizo.');
+        ifrm.setAttribute('same', '');
+        ifrm.setAttribute('class', 'bg-background mx-auto');
+  
+        entry.target.appendChild(ifrm)
+    
+        ifrm.setAttribute('src', 'https://perrodinero.substack.com/embed');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  observer.observe(embed);
+}, false);
