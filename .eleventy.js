bc829f42828b597083b5459860fb91744065b9e8
David Ojeda <david.ojeda.lopez@gmail.com>
2020-12-09 18:46:54 -0600

    Remove tags sections

--- .eleventy.js
+++ .eleventy.js
@@ -43,8 +43,6 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
-
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy('favicon.ico')
