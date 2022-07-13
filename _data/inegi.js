81cee916310422924df6fbae6421d4372a170b1d
David <david.ojeda@nubank.com.br>
2022-07-12 23:22:41 -0600

    Update latest UMA values from INEGI

--- _data/inegi.js
+++ _data/inegi.js
@@ -1,19 +1,19 @@
 module.exports = async function () {
     return {
         uma_daily: {
-            amount: 89.62,
+            amount: 96.22,
             description: 'Valor diario de la UMA',
-            for_year: 2021
+            for_year: 2022
         },
         uma_monthly: {
-            amount: 2724.45,
+            amount: 2925.09,
             description: 'Valor mensual de la UMA. Se calcula multiplicando su valor diario por 30.4 veces',
-            for_year: 2021
+            for_year: 2022
         },
         uma_yearly: {
-            amount: 32693.40,
+            amount: 35101.08,
             description: 'Valor anual de la UMA. Se calcula multiplicando su valor mensual por 12',
-            for_year: 2021
+            for_year: 2022
         },
     }
 }
