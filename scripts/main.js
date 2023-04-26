4538aea601fd1f9e87735fabaa1d4da538b06b95
David Ojeda <david.ojeda.lopez@gmail.com>
2023-04-26 00:07:54 -0700

    Fix newsletter iframe

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,32 @@ import { annotate } from 'rough-notation'
 window.addEventListener("load", (event) => {
   setDarkModeToggle();
   setAnnotations();
+  setNewsletterObserver();
 }, false);
+
+function setNewsletterObserver() {
+  const newsletterCta = document.querySelector('.newsletter-cta');
+  const iframe = newsletterCta.querySelector('iframe.newsletter-cta-iframe');
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        iframe.src = iframe.dataset.src;
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  observer.observe(newsletterCta);
+}
 
 function setAnnotations() {
   const annotations = document.querySelectorAll('.annotated');
