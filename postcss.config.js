c3498dc64964ca281718d3b6cbd8899027cecf7b
David Ojeda <davidojeda@hey.com>
2026-06-03 18:05:01 -0600

    fix: emit sRGB colors for Lighthouse

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
+    '@csstools/postcss-oklab-function': {
+      preserve: false,
+      subFeatures: { displayP3: false },
+    },
     autoprefixer: {},
   }
 }
